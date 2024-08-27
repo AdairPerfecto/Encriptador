f3f4a5b0d01e76546a7ffcb005f2a30e1d7cb9fa
AdairPerfecto <adairperfecto@gmail.com>
2024-08-26 21:40:56 -0600

    Proyecto encriptador

--- script.js
+++ script.js
@@ -1,2 +1,52 @@
-//Desafíos:
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let enigma = document.getElementById("enigma");
+
+    let textoCifrado = texto
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat")
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con éxito";
+        parrafo.textContent = "";
+        enigma.src = "./imagenes/encriptado.jpg";
+    } else {
+        enigma.src = "./imagenes/enigma.png";
+        tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let enigma = document.getElementById("enigma");
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u")
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        enigma.src = "./imagenes/desencriptado.png";
+    } else {
+        enigma.src = "./imagenes/enigma.png";
+        tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
 
